93713f0ebc6c33db3211d138355c2717056ebdc7
David Morais <104798002+SirD007@users.noreply.github.com>
2022-09-17 22:45:21 -0300

    Co-authored-by: Wallison Santos <KYOWS@users.noreply.github.com> Co-authored-by: RrR2010 <RrR2010@users.noreply.github.com>

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -8,10 +8,26 @@ function App() {
     <div className='flex flex-col'>
       <TopBar/>
       <div className='grid grid-cols-2'>
-        <SwotItem/>
-        <SwotItem/>
-        <SwotItem/>
-        <SwotItem/>
+        <SwotItem
+        title='Forças'
+        icon='fitness_center'
+        backgroundColor='bg-green-500'
+        />
+        <SwotItem
+        title='Fraquezas'
+        icon='link_off'
+        backgroundColor='bg-red-500'
+        />
+        <SwotItem
+        title='Oportunidades'
+        icon='star'
+        backgroundColor='bg-blue-500'
+        />
+        <SwotItem
+        title='Ameaças'
+        icon='warning'
+        backgroundColor='bg-yellow-500'
+        />
       </div>
     </div>
   )
--- client/src/components/SwotItem.tsx
+++ client/src/components/SwotItem.tsx
@@ -1,13 +1,21 @@
-import { SwotItemHeader } from "./SwotItemTitle";
+interface SwotItemProps {
+    title: string;
+    icon: string;
+    backgroundColor: string;
+}
 
-export function SwotItem() {
-    return(
-        <div className="w-full h-full bg-white border-zinc-200 rounded-lg shadow-md p-4">
-            <SwotItemHeader
-            title='Forças'
-            icon='fitness_center'
-            backgroundColor='bg-green-400'
-            />
+export function SwotItem(props: SwotItemProps){
+    return (
+        <div className="h-full w-full border-zinc-200 rounded-lg shadow-md p-4 flex flex-col">
+
+            <div className="flex items-center gap-2">
+                <span className={`materials-symbols-outlined ${props.backgroundColor} rounded-full p-4`}>{props.icon}</span>
+                <span>{props.title}</span>
+            </div>
+
+            <div>
+                
+            </div>
         </div>
 
     )
