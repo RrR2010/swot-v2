6140d39b49336058e4d505fea19acb432df8a224
RrR2010 <richard.rsmi@hotmail.com>
2022-09-25 01:29:36 -0300

    inserted token generation and password hash

--- server/prisma/migrations/20220925025037_create_tables/migration.sql
+++ server/prisma/migrations/20220925025037_create_tables/migration.sql
@@ -2,12 +2,16 @@
 CREATE TABLE "User" (
     "id" TEXT NOT NULL PRIMARY KEY,
     "name" TEXT NOT NULL,
-    "mail" TEXT NOT NULL
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 -- CreateTable
 CREATE TABLE "Project" (
     "id" TEXT NOT NULL PRIMARY KEY,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "name" TEXT NOT NULL,
     "authorId" TEXT NOT NULL,
     CONSTRAINT "Project_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
 );
@@ -17,7 +21,8 @@ CREATE TABLE "FactorType" (
     "id" TEXT NOT NULL PRIMARY KEY,
     "name" TEXT NOT NULL,
     "pluralName" TEXT NOT NULL,
-    "symbol" TEXT NOT NULL
+    "symbol" TEXT NOT NULL,
+    "colorHex" TEXT NOT NULL
 );
 
 -- CreateTable
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,43 +1,102 @@
-import express from 'express'
+import express, { json } from 'express'
 import { PrismaClient } from '@prisma/client'
+import jwt from 'jsonwebtoken'
+import bcrypt from 'bcryptjs'
+
+const authConfig = require('../src/config/auth')
 
 const app = express()
 const prisma = new PrismaClient()
 
 app.use(express.json())
 
+app.post('/signup', async (req, res) => {
+  console.log('A')
+  const { name, email, password } = req.body;
+  const user = await prisma.user.findFirst({
+    where: { email }
+  })
+
+  if (user) {
+    return res.status(401).json({ error: 'Email already exists' })
+  }
+
+  try {
+    const hashedPassword = await bcrypt.hash(password, 10)
+    const newUser = await prisma.user.create({
+      data: {
+        name,
+        email,
+        password: hashedPassword
+      }
+    })
+    return res.status(200).json({ newUser })
+  } catch {
+    return res.status(401).json({ error: 'Error creating user' })
+  }
+
+
+})
+
+app.post('/signin', async (req, res) => {
+  const { email, password } = req.body;
+
+  let user = await prisma.user.findFirst({
+    where: { email },
+    // select: { id: true, email: true, password: true }
+  })
+
+  if (!user) {
+    return res.status(401).json({ error: 'User not found' })
+  }
+
+  if (!await bcrypt.compare(password, user.password)) {
+    return res.status(401).json({ error: 'Incorrect password' })
+  }
+
+  const token = jwt.sign(
+    { id: user.id },
+    authConfig.secret,
+    { expiresIn: authConfig.expiresIn }
+  )
+
+  return res.status(200).json({ user: user, token: token })
+
+})
+
+
 app.get('/projects', async (req, res) => {
   const projects = await prisma.project.findMany({
-    orderBy: {createdAt: 'desc'}
+    orderBy: { createdAt: 'desc' }
   })
   return res.json(projects)
 })
 
 app.get('/projects/:id', async (req, res) => {
   const project = await prisma.project.findFirstOrThrow({
-    where: {id: req.params.id}
+    where: { id: req.params.id }
   })
   return res.json(project)
 })
 
 app.get('/users', async (req, res) => {
   const users = await prisma.user.findMany({
-    orderBy: {name: 'asc'}
+    orderBy: { name: 'asc' }
   })
   return res.json(users)
 })
 
 app.get('/users/:id', async (req, res) => {
   const users = await prisma.user.findFirstOrThrow({
-    where: {id: req.params.id}
+    where: { id: req.params.id }
   })
   return res.json(users)
 })
 
 app.get('/users/:id/projects', async (req, res) => {
   const projects = await prisma.project.findMany({
-    where: {authorId: req.params.id},
-    orderBy: {createdAt: 'desc'},
+    where: { authorId: req.params.id },
+    orderBy: { createdAt: 'desc' },
   })
   return res.json(projects)
 })
